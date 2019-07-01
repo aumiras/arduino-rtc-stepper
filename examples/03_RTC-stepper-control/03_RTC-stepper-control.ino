83435309e9bdeb6e3411d3932492ab3b6ee0f3cf
mir <aurimas.gaizauskas@gmail.com>
2019-07-01 23:37:23 +0300

    Update 03_RTC-stepper-control.ino

--- examples/03_RTC-stepper-control/03_RTC-stepper-control.ino
+++ examples/03_RTC-stepper-control/03_RTC-stepper-control.ino
@@ -1,41 +1,27 @@
-/**
- * Author:Ab Kurk
- * version: 1.0
- * date: 4/02/2018
- * Description:
- * This sketch is part of the guide to putting your Arduino to sleep
- * tutorial. We use the:
- * Adafruit DS3231 RTC
- * Adafruit 5V ready Micro SD break out board
- * Arduino Pro Mini
- * DHT11 or DHT22 humidety/Temperature sensor
- * In this example we use the RTC to wake up the Arduino to log the temp and humidity on to an SD card.
- * After the data has been logged the Arduino goes back to sleep and gets woken up 5 minutes later to
- * start all over again
- * Link To Tutorial http://www.thearduinomakerman.info/blog/2018/1/24/guide-to-arduino-sleep-mode
- * Link To Project   http://www.thearduinomakerman.info/blog/2018/2/5/wakeup-rtc-datalogger
+/*
+ The code is based on famous Ab Kurk tutorial on putting Arduino Uno to sleep with Adafruit DS3231 RTC, excluded Adafruit 5V ready Micro SD break out board and DHT11 or DHT22 humidety/Temperature sensor
+ Link To Tutorial http://www.thearduinomakerman.info/blog/2018/1/24/guide-to-arduino-sleep-mode
+ Link To Project   http://www.thearduinomakerman.info/blog/2018/2/5/wakeup-rtc-datalogger
  */
 
-#include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
+#include <avr/sleep.h>
 #include <DS3232RTC.h>  //RTC Library https://github.com/JChristensen/DS3232RTC
 
 const byte interruptPin = 2;
 int time_interval = 1;
-int smDirectionPin = 12;
+int smDirectionPin = 12; // stepper pins setup
 int smStepPin = 13;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("initialize pins.");
   
-  pinMode(LED_BUILTIN,OUTPUT);//We use the led on pin 13 to indecate when Arduino is A sleep
-  pinMode(interruptPin,INPUT_PULLUP);//Set pin d2 to input using the buildin pullup resistor
-  digitalWrite(LED_BUILTIN,HIGH);//turning LED on
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(interruptPin,INPUT_PULLUP);
+  digitalWrite(LED_BUILTIN,HIGH);     //turning LED on
 
-  pinMode(smDirectionPin, OUTPUT);
+  pinMode(smDirectionPin, OUTPUT); // stepper pinMode declaration
   pinMode(smStepPin, OUTPUT);
-
-  //RTC.begin();
 
   // initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
     RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
@@ -45,43 +31,29 @@ void setup() {
     RTC.alarmInterrupt(ALARM_1, false);
     RTC.alarmInterrupt(ALARM_2, false);
     RTC.squareWave(SQWAVE_NONE);
-    /*
-     * Uncomment the block block to set the time on your RTC. Remember to comment it again
-     * otherwise you will set the time at everytime you upload the sketch
-     * /
-     /* Begin block
-     tmElements_t tm;
-    tm.Hour = 00;               // set the RTC to an arbitrary time
-    tm.Minute = 00;
-    tm.Second = 00;
-    tm.Day = 4;
-    tm.Month = 2;
-    tm.Year = 2018 - 1970;      // tmElements_t.Year is the offset from 1970
-    RTC.write(tm);              // set the RTC from the tm structure
-     Block end * */
-     time_t t; //create a temporary time variable so we can set the time and read the time from the RTC
-    t=RTC.get();//Gets the current time of the RTC
-    int alarmDuration = minute(t)+time_interval;
-    RTC.setAlarm(ALM1_MATCH_MINUTES , 0, minute(t)+time_interval, 0, 0);// Setting alarm 1 to go off 5 minutes from now
-    Serial.println(alarmDuration);//prints time stamp on serial monitor
-    // clear the alarm flag
-    RTC.alarm(ALARM_1);
-    // configure the INT/SQW pin for "interrupt" operation (disable square wave output)
-    RTC.squareWave(SQWAVE_NONE);
-    Serial.println("enable interrupt output for Alarm 1.");// enable interrupt output for Alarm 1
+  
+    time_t t;       //create a temporary time variable so we can set the time and read the time from the RTC
+    t=RTC.get();    //Gets the current time of the RTC
+    int alarmDuration = minute(t)+time_interval;    // calculate Alarm minutes
+    RTC.setAlarm(ALM1_MATCH_MINUTES , 0, alarmDuration, 0, 0);    // Setting alarm1 to go off time_interval from now /check code 11 line
+    Serial.println(alarmDuration);    //prints Alarm minutes on serial monitor
+    
+    RTC.alarm(ALARM_1);   // clear the alarm flag
+    RTC.squareWave(SQWAVE_NONE);    // configure the INT/SQW pin for "interrupt" operation (disable square wave output)
+    Serial.println("enable interrupt output for Alarm 1.");   // enable interrupt output for Alarm 1
     RTC.alarmInterrupt(ALARM_1, true);
 
     Serial.println("initialization done.");
 }
 
 void loop() {
- delay(5000);//wait 5 seconds before going to sleep. In real senairio keep this as small as posible
+ delay(5000);   //  wait 5 seconds before going to sleep. In real scenairio keep this as small as posible
  Going_To_Sleep();
 }
 
 void Going_To_Sleep(){
-    sleep_enable();//Enabling sleep mode
-    attachInterrupt(digitalPinToInterrupt(interruptPin), wakeUp, LOW); //attachInterrupt(0, wakeUp, LOW);//attaching a interrupt to pin d2
+    sleep_enable();   //  Enabling sleep mode
+    attachInterrupt(digitalPinToInterrupt(interruptPin), wakeUp, LOW);    //  attachInterrupt(0, wakeUp, LOW);//attaching a interrupt to pin d2
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sleep
     digitalWrite(LED_BUILTIN,LOW);//turning LED off
     time_t t;// creates temp time variable
